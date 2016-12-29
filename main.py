b36832c2637e7fe3c1800d66de7bde6f75d17630
GreatDrake <nikich_morozov@mail.ru>
2016-12-29 21:43:43 +0300

    Added menubar and "About" dialog window

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from PyQt5.QtWidgets import (QPushButton, QFormLayout, QWidget, QLineEdit, QLabel, 
-                             QVBoxLayout, QMessageBox, QApplication)
+                             QVBoxLayout, QMessageBox, QMainWindow, QAction, QDialog, qApp)
 from PyQt5.QtGui import QFont, QIcon
 from PyQt5.QtCore import Qt
 from downloader import Downloader
-import sys
 
-class Window(QWidget):
+class UI(QWidget):
     def __init__(self):
         super().__init__()
         
         self.initUI()
         
     def initUI(self):
-        
         self.lbl = QLabel('URL: ')
-        self.lbl.setFont(QFont('Calibri', 18))
+        self.lbl.setFont(QFont('Calibri', 17))
         
         self.le = QLineEdit(self)
-        self.le.setMinimumHeight(45)
+        self.le.setMinimumHeight(40)
         self.le.setMinimumWidth(500)
         self.le.setFont(QFont('Calibri', 16))
         
@@ -25,7 +23,6 @@ class Window(QWidget):
         self.btn.setFont(QFont('Calibri', 12))
         self.btn.setFixedWidth(200)
         self.btn.setFixedHeight(40)
-        self.btn.clicked.connect(self.getImages)
         
         self.form = QFormLayout()
         self.form.addRow(self.lbl, self.le)
@@ -40,43 +37,87 @@ class Window(QWidget):
         
         self.setLayout(self.h)
         
-        self.setWindowTitle('ImageGetter')
-        self.setWindowIcon(QIcon('arrow.ico'))
+                
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
         
+        self.ui = UI()
+        self.setCentralWidget(self.ui)
+        self.ui.btn.clicked.connect(self.getImages)
+        
+        menubar = self.menuBar()
+        exitAction = QAction("Exit", self)
+        exitAction.triggered.connect(qApp.quit)
+        fileMenu = menubar.addMenu("File")
+        fileMenu.addAction(exitAction)
+        aboutAction = QAction("About", self)
+        aboutAction.triggered.connect(self.showInfo)
+        aboutMenu = menubar.addMenu("Help")
+        aboutMenu.addAction(aboutAction)
+
+        self.setWindowTitle('ImageDownloader')
+        self.setWindowIcon(QIcon('arrow.ico'))
         self.show()
-        self.resize(self.width(), self.height()*2)
+        self.resize(self.width(), self.height() * 1.8)
         self.setMaximumSize(self.width() * 1.5, self.height() * 2)
-        self.le.setMinimumWidth(self.le.width() / 1.5)
+        self.ui.le.setMinimumWidth(self.ui.le.width() / 1.5)
         
     def getImages(self):
-        if self.le.text().isspace() or not self.le.text():
+        if self.ui.le.text().isspace() or not self.ui.le.text():
             return
         
-        self.temp = self.le.text()
-        self.le.setText('Searching...')
+        self.temp = self.ui.le.text()
+        self.ui.le.setText('Searching...')
         self.th = Downloader(self.temp)
         self.th.finished[int].connect(self.onFinished)
         self.th.error.connect(self.onError)
         self.th.start()
         
     def onError(self):
-        self.le.setText(self.temp)
+        self.ui.le.setText(self.temp)
         box = QMessageBox(self)
-        box.setWindowTitle('done')
+        box.setWindowTitle('Error')
         box.setText('Error occured while accessing URL.')
         box.setIcon(QMessageBox.Critical)
         box.setFont(QFont('Calibri', 13))
         box.setStandardButtons(QMessageBox.Ok)
         box.exec_()
-        
 
     def onFinished(self, i):
-        self.le.setText(self.temp)
+        self.ui.le.setText(self.temp)
         box = QMessageBox(self)
-        box.setWindowTitle('done')
+        box.setWindowTitle('Done') 
         box.setText('Got ' + str(i) + ' images.')
         box.setIcon(QMessageBox.Information)
         box.setFont(QFont('Calibri', 13))
         box.setStandardButtons(QMessageBox.Ok)
         box.exec_()
+        
+    def showInfo(self):
+        about = About(self.x() + self.width()/2, self.y() + self.height()/2)
+        about.setWindowFlags(Qt.Window)
+        about.exec_()
+        
+class About(QDialog):
+    def __init__(self, x, y):
+        super().__init__()
+        
+        self.lbl = QLabel("ImageDownloader\nVersion 1.0\n\n©Nitita Morozov 2017", self)
+        self.lbl.setFont(QFont('Calibri', 16))
+        self.lbl.move(10, 10)
+        
+        self.setStyleSheet("background-color: white;")
+        self.setWindowTitle("About")
+        self.setWindowIcon(QIcon('arrow.ico'))
+        self.resize(300, 190)
+        self.move(x - self.width() / 2, y - self.height() / 2)
+        self.show()
+        
+        
+        
+        
+        
+        
+        
         
