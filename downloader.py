ce100a8965977f6e407ec1a334a7f6a6924c582a
GreatDrake <nikich_morozov@mail.ru>
2016-12-29 21:44:26 +0300

    A few improvements to parser

--- downloader.py
+++ downloader.py
@@ -1,6 +1,6 @@
 from PyQt5.QtCore import QThread, pyqtSignal
 from urllib.request import urlopen
-from urllib.parse import urlparse, urljoin
+from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import os
 import shutil
@@ -14,57 +14,45 @@ class Downloader(QThread):
         self.text = text
     
     def run(self):
-        
-        path = self.text
+        url = self.text
        
         try:
-            parsed_uri = urlparse(path)
-            self.protocol = parsed_uri.scheme
-            domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
-        except Exception:
-            self.error.emit()
-            return
-        try:
-            html = urlopen(path)
+            html = urlopen(url)
             bsObj = BeautifulSoup(html, 'html.parser')
             images = bsObj.findAll("img") 
         except Exception:
             self.error.emit()
             return
-            
+        
         if not ('images' in os.listdir(os.getcwd())):
             os.mkdir('images')
         else:
             shutil.rmtree('images')
             os.mkdir('images')
 
-        i = 0
+        c = 0
         for image in images:
+            pth = self.getCorrectPath(image, url)
+            c += 1
             try:
-                pth = self.getCorrectPath(image, path, domain)
-            except Exception:
-                self.error.emit()
-                return
-            try:
-                i += 1
                 with urlopen(pth) as f:
                     im = f.read()
                     if im:
-                        with open(os.path.join('images', str(i))+ '.png', 'wb') as img:
+                        with open(os.path.join('images', str(c))+ '.png', 'wb') as img:
                             img.write(im)
                     else:
-                        i -= 1
+                        c -= 1
             except Exception:
-                i -= 1
-        self.finished.emit(i)
+                c -= 1
+                
+        self.finished.emit(c)
             
-    def getCorrectPath(self, image, path, domain):
-        if '..' in image['src']:
-            path2 = os.path.dirname(path) + image['src']
-            i2 = path2.find('..')
-            i1 = path2.rindex('/', 0, i2)
-            return path2[0:i1] + path2[i2+2:]
-        elif 'http' in image['src']:
+    def getCorrectPath(self, image, url):
+        if 'http' in image.attrs['src']:
             return image.attrs['src']
         else:
-            return (domain + image.attrs['src'] if not image.attrs['src'][0:2] == '//' else self.protocol + ':' + image['src'])
+            return urljoin(url, image.attrs['src'])
+        
+        
+        
+        
